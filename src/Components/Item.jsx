fc68cc27290ef8864e5c671bd225e19181b0f87e
EmilyM <155667566+EmilyMenken@user.noreply.github.com>
2025-10-30 14:12:35 -0700

    changes to css to see img and txt next to each other

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -1,18 +1,27 @@
-
-export default function Item({data}) {
+export default function Item({ data }) {
     console.log(data)
+    
     return (
-        <div className = "imgData">
+        <div className="imgData">
 
             <h2>{data[0].title}</h2>
-            <p>{data[0].date}</p>
+            <h5>{data[0].date}</h5>
 
             <a href={data[0].url} target="_blank">Click here for a full screen view!</a>
+            
+            <div className="contentRow">
 
-            <img src ={data[0].url}/>
-            <p>{data[0].explanation}</p>
+                <div className="imgs">
+                    <img src={data[0].url} />
+                </div>
 
-            <button> Skip to next picture </button>
+                <div className="txts">
+                    <p>{data[0].explanation}</p>
+                </div>
+
+            </div>
+
+            <button>Skip to next picture</button>
 
         </div>
     )
