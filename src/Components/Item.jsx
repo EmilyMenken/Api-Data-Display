c2e7dbda6702b05b751463beaae4adb980efcc85
EmilyM <155667566+EmilyMenken@user.noreply.github.com>
2025-10-30 14:35:11 -0700

    changes to css

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -4,14 +4,23 @@ export default function Item({ data, onNext, onPrev }) {
   return (
     <div className="imgData">
       <h2>{data.title}</h2>
-      <p>{data.date}</p>
+      <h5>{data.date}</h5>
 
       <a href={data.url} target="_blank" rel="noreferrer">
-        Click here for a full screen view!
+        Click here for a full screen view of the current image!
       </a>
 
-      <img src={data.url} alt={data.title} />
-      <p>{data.explanation}</p>
+      <div className="contentRow">
+
+        <div className="imgs">
+          <img src={data.url} alt={data.title} />
+        </div>
+
+        <div className="txts">
+          <p>{data.explanation}</p>
+        </div>
+
+      </div>
 
       <div className="buttons">
         <button onClick={onPrev}>Previous</button>
